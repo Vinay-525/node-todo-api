3e246e9bee16004faf4df246833128cb00366fd0
Vinay Bansal <vinaybansalvinay@gmail.com>
2018-06-02 00:39:27 +0530

    Test POST/todo route

--- server/server.js
+++ server/server.js
@@ -8,30 +8,32 @@ var {User} = require('./models/user')
 var app = express();
 app.use(bodyParser.json());
 
-// app.post('/todos',(req,res) => {
-//   var todos = new Todo({
-//     text: req.body.text
-//   })
-//
-//   todos.save().then((doc) => {
-//     res.send(doc)
-//   },(e) => {
-//     res.send(e)
-//   })
-// })
-app.post('/users',(req,res) => {
-  var users = new User({
-    email: req.body.email
-  })
-  users.save().then((user) => {
-    res.send(user)
-  },(e)=> {
-    res.send(e)
+app.post('/todos',(req,res) => {
+  var todos = new Todo({
+    text: req.body.text
   })
 
+  todos.save().then((doc) => {
+    res.send(doc)
+  },(e) => {
+    res.status(400).send(e)
+  })
 })
+// app.post('/users',(req,res) => {
+//   var users = new User({
+//     email: req.body.email
+//   })
+//   users.save().then((user) => {
+//     res.send(user)
+//   },(e)=> {
+//     res.send(e)
+//   })
+//
+// })
 
 
 app.listen(3000,() => {
   console.log('Server is up on port 3000');
 })
+
+module.exports = {app}
