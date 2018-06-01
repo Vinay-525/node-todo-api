cc0d6d270c3eade0352fddb89eb2f2a68074cb4b
Vinay Bansal <vinaybansalvinay@gmail.com>
2018-06-01 21:54:33 +0530

    Adding POST Route and refractor mongoose

---
+++ server/server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var {mongoose} = require('./db/mongoose')
+var {Todo} = require('./models/todos')
+var {User} = require('./models/user')
+
+var app = express();
+app.use(bodyParser.json());
+
+// app.post('/todos',(req,res) => {
+//   var todos = new Todo({
+//     text: req.body.text
+//   })
+//
+//   todos.save().then((doc) => {
+//     res.send(doc)
+//   },(e) => {
+//     res.send(e)
+//   })
+// })
+app.post('/users',(req,res) => {
+  var users = new User({
+    email: req.body.email
+  })
+  users.save().then((user) => {
+    res.send(user)
+  },(e)=> {
+    res.send(e)
+  })
+
+})
+
+
+app.listen(3000,() => {
+  console.log('Server is up on port 3000');
+})
